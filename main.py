a4cc3a09838aad06df198e11040362557b8682c7
syd-cavs <sydney.cavallaro@gmail.com>
2025-08-21 16:07:58 -0400

    G: added collisions method to circleshape

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from constants import *
 from player import Player
@@ -27,12 +28,17 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collisions(player):
+                print("Game over!")
+                sys.exit()
+
         screen.fill("black")
         for pic in drawable:
             pic.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
 
 if __name__ == "__main__":
     main()
